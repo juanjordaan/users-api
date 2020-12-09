dbdd2e24f320405e674635f989ced94d2b645fef
Juan Jordaan <juan.jordaan1234@gmail.com>
2020-12-09 17:17:24 +0200

    remove comments

--- src/main/java/cloud/jordaan/juan/kinetic/application/exception/CheckedExceptionWrapper.java
+++ src/main/java/cloud/jordaan/juan/kinetic/application/exception/CheckedExceptionWrapper.java
@@ -1,5 +1,6 @@
 package cloud.jordaan.juan.kinetic.application.exception;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public interface CheckedExceptionWrapper {
@@ -7,6 +8,14 @@ public interface CheckedExceptionWrapper {
 	public static <R, T> R thowsRuntimeException(Function<T, R> f, T t) {
 		try {
 			return f.apply(t);
+		} catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static <R, T, U> R thowsRuntimeException(BiFunction<T, U, R> f, T t, U u) {
+		try {
+			return f.apply(t, u);
 		} catch(Exception e) {
 			throw new RuntimeException(e);
 		}
--- src/main/java/cloud/jordaan/juan/kinetic/infrastructure/security/Jwt.java
+++ src/main/java/cloud/jordaan/juan/kinetic/infrastructure/security/Jwt.java
@@ -4,8 +4,5 @@ public class Jwt {
 	public static final String XAUTH_TOKEN_TYPE = "X-Auth";
 	public static final String BEARER_TOKEN_TYPE = "Bearer ";
 	public static final String ISSUER = "juan.jordaan.cloud";
-
-    //public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 5*60*60;
-//    public static final String SIGNING_KEY = "devglan123r";
     public static final String AUTHORITIES_KEY = "scopes";
 }
